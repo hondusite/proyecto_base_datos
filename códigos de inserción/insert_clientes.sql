675f73e4458a4ce5abdfaddb1f027a24106870ed
German Solorzano <hondusite@gmail.com>
2025-08-07 12:24:41 -0600

    cambio en sql

--- códigos de inserción/insert_clientes.sql
+++ códigos de inserción/insert_clientes.sql
@@ -3,3 +3,6 @@ INSERT INTO clientes (id, nombre, email, telefono) VALUES
 (1, 'Juan Pérez', 'juan.perez@example.com', '555-1234'),
 (2, 'María Gómez', 'maria.gomez@example.com', '555-5678'),
 (3, 'Luis Martínez', 'luis.martinez@example.com', '555-8765');
+--insertar datos en la tabla clientes
+INSERT INTO clientes (id, nombre, email, telefono) VALUES
+(4, 'Ana Torres', 'ana.torres@example.com', '555-4321');
